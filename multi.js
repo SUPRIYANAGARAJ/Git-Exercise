abe15998b6f966810264947f5a81f1fa8ff20452
Supriya SN <98803360+SUPRIYANAGARAJ@users.noreply.github.com>
2022-03-25 11:58:11 +0530

    Change add to mult

--- multi.js
+++ multi.js
@@ -1,5 +1,5 @@
 function pri(a,b){                      
     
-    return a + b ;                                                              
+    return a * b ;                                                              
 }              
  pri(2,3);
